26b2bbbb9fcf3cd961ca206448a4b01d6e424417
Kenta Sato <karupa@cpan.org>
2011-06-28 15:14:18 +0900

    initial commit

---
+++ lib/Plack/App/Directory/Xslate.pm
@@ -0,0 +1,32 @@
+package Plack::App::Directory::Xslate;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+Plack::App::Directory::Xslate -
+
+=head1 SYNOPSIS
+
+  use Plack::App::Directory::Xslate;
+
+=head1 DESCRIPTION
+
+Plack::App::Directory::Xslate is
+
+=head1 AUTHOR
+
+Kenta Sato E<lt>karupa@cpan.orgE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
