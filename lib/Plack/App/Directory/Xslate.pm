10a6ee64f2729b417ce1693a9947a24d540beb4a
Kenta Sato <karupa@cpan.org>
2011-09-11 16:16:24 +0900

    テストを追加＆常に渡すパラメータを指定出来るようにした。

--- lib/Plack/App/Directory/Xslate.pm
+++ lib/Plack/App/Directory/Xslate.pm
@@ -1,20 +1,22 @@
 package Plack::App::Directory::Xslate;
 use strict;
 use warnings;
-our $VERSION = '0.03';
+our $VERSION = '0.04';
 
 use parent qw(Plack::App::Directory);
 use Text::Xslate;
-use Plack::Util::Accessor qw(xslate_opt xslate_path);
+use Plack::Util::Accessor qw(xslate_opt xslate_path xslate_param);
 use Encode;
+use Plack::Util;
 
 sub new{
 	my $class = shift;
 	my $self  = $class->SUPER::new(@_);
 
 	$self->xslate_opt->{path} = $self->root;
-	$self->{xslate} = Text::Xslate->new($self->xslate_opt);
-	$self->{encoder} = find_encoding('utf-8');
+	$self->{xslate}  = Text::Xslate->new($self->xslate_opt);
+	$self->{encoder} = Encode::find_encoding('utf-8');
+    $self->xslate_param(+{}) unless $self->xslate_param;
 
 	return $self;
 }
@@ -22,14 +24,14 @@ sub new{
 {
 	no strict 'refs';
 	no warnings 'redefine';
-	my $orig = \&Plack::App::File::serve_path;
+	my $orig = Plack::App::File->can('serve_path');
 	*Plack::App::File::serve_path = sub{
         my($self, $env) = @_;
 
 		my $match = 0;
 
 		my $path_match = $self->xslate_path;
-		if($path_match){
+		if ($path_match) {
 			$match = 1;
 			for my $path ($env->{SCRIPT_NAME}){
 				unless(('CODE' eq ref($path_match)) ? $path_match->($path) : $path =~ $path_match){
@@ -40,39 +42,48 @@ sub new{
 		}
 
 		return $match ?
-			[
-				200,
-				['Content-Type' => 'text/html'],
-				[$self->{encoder}->encode(
-				     $self->{xslate}->render($env->{SCRIPT_NAME}, +{})
-				)]
-			]:  
+			do {
+                my $content = $self->{encoder}->encode(
+                    $self->{xslate}->render($env->{SCRIPT_NAME}, $self->xslate_param)
+                );
+                [
+                 200,
+                 [
+                  'Content-Type'   => 'text/html',
+                  'Content-Length' => Plack::Util::content_length($content),
+                 ],
+                 [$content]
+                ]
+            }:
 			$orig->(@_);
 	}
 }
 
-1; 
-__END__ 
+1;
+__END__
 
 =head1 NAME
 
-Plack::App::Directory::Xslate - Serve static files and Text::Xslate template files from document root with directory index
+  Plack::App::Directory::Xslate - Serve static files and Text::Xslate template files from document root with directory index
 
 =head1 SYNOPSIS
 
-# app.psgi
-use Plack::App::Directory::Xslate;
-my $app = Plack::App::Directory::Xslate->new({
+  # app.psgi
+  use Plack::App::Directory::Xslate;
+  my $app = Plack::App::Directory::Xslate->new({
     root => "/path/to/htdocs",
     xslate_opt  => +{ # Text::Xslate->new()
         syntax => 'TTerse',
+    },
+    xslate_param => +{
+        hoge => 'fuga',
     },
     xslate_path => qr{\.tt$},
  })->to_app;
 
 =head1 DESCRIPTION
 
-This is a static files and Text::Xslate template files server PSGI application with directory index a la Apache's mod_autoindex.
+  This is a static files and Text::Xslate template files server PSGI application with directory index a la Apache's mod_autoindex.
 
 =head1 CONFIGURATION
 
@@ -80,21 +91,25 @@ This is a static files and Text::Xslate template files server PSGI application w
 
 =item root
 
-Document root directory. Defaults to the current directory.
+  Document root directory. Defaults to the current directory.
 
 =item xslate_opt
 
-Text::Xslate constructor option.
+  Text::Xslate constructor option.
 
 =item xslate_path : Regexp or CodeRef
 
-Allow Text::Xslate rendering path.
+  Allow Text::Xslate rendering path.
+
+=item xslate_param : HashRef
+
+  Text::Xslate rendering variables.
 
 =back
 
 =head1 AUTHOR
 
-Kenta Sato E<lt>karupa@cpan.orgE<gt>
+  Kenta Sato E<lt>karupa@cpan.orgE<gt>
 
 =head1 SEE ALSO
 
---
+++ t/01_basic.t
@@ -0,0 +1,38 @@
+use strict;
+use warnings;
+
+use Test::More tests => 1;
+use Plack::Test;
+use Plack::App::Directory::Xslate;
+use HTTP::Request;
+
+my $app = Plack::App::Directory::Xslate->new({
+    root => 't/htdocs/',
+    xslate_opt  => +{
+        cache => 0,
+    },
+    xslate_param => +{
+        hoge => 'fuga',
+    },
+    xslate_path => qr{\.tx$},
+})->to_app;
+
+subtest 'Plack::Test' => sub {
+    plan tests => 2;
+    test_psgi
+        app => $app,
+        client => sub {
+            my $cb = shift;
+            my $req = HTTP::Request->new(GET => 'http://localhost/test.txt');
+            my $res = $cb->($req);
+            is $res->content, 'Test <: $hoge :>', 'nomarl file';
+        };
+    test_psgi
+        app => $app,
+        client => sub {
+            my $cb = shift;
+            my $req = HTTP::Request->new(GET => 'http://localhost/test.tx');
+            my $res = $cb->($req);
+            is $res->content, 'Test fuga', 'template file';
+        };
+};
